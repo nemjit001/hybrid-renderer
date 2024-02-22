bcaade4eee0756debe007e94204bdb604a0e21d4
Tijmen Verhoef <tijmenmenno@gmail.com>
2024-02-22 10:45:14 +0100

    created GPU Mesh structure for handling device buffer resources for mesh

--- hybrid-renderer-lib/include/mesh.h
+++ hybrid-renderer-lib/include/mesh.h
@@ -4,6 +4,8 @@
 #include <vector>
 
 #include "hri_math.h"
+#include "renderer_internal/render_context.h"
+#include "renderer_internal/buffer.h"
 
 namespace hri
 {
@@ -14,6 +16,27 @@ namespace hri
 		Float3 normal;
 		Float3 tangent;
 		Float2 textureCoord;
+	};
+
+	/// @brief The GPU Mesh struct contains host visible vertex & index buffers as well as metadata.
+	struct GPUMesh
+	{
+		uint32_t indexCount				= 0;
+		uint32_t vertexCount			= 0;
+		BufferResource vertexBuffer		= BufferResource{};
+		BufferResource indexBuffer		= BufferResource{};
+
+		/// @brief Initialize a new GPU Mesh.
+		/// @param ctx Render Context to use.
+		/// @param vertices Vertex array.
+		/// @param indices Index array.
+		/// @return A new GPU Mesh.
+		static GPUMesh init(RenderContext* ctx, const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices);
+
+		/// @brief Destroy a GPU Mesh.
+		/// @param ctx Render context used to create the GPU Mesh.
+		/// @param mesh Mesh to destroy.
+		static void destroy(RenderContext* ctx, GPUMesh& mesh);
 	};
 
 	/// @brief A Mesh represents a renderable object. It uses indexed drawing to reduce vertex array size.
@@ -29,6 +52,11 @@ namespace hri
 
 		/// @brief Destroy this Mesh object.
 		virtual ~Mesh() = default;
+
+		/// @brief Create a GPU Mesh object from this mesh, allocating device buffers & setting metadata.
+		/// @param ctx Render Context to use.
+		/// @return A new GPUMesh.
+		GPUMesh createGPUMesh(RenderContext* ctx);
 
 	public:
 		std::vector<Vertex> vertices	= {};
--- hybrid-renderer-lib/src/mesh.cpp
+++ hybrid-renderer-lib/src/mesh.cpp
@@ -1,6 +1,40 @@
 #include "mesh.h"
 
+#include <cstdint>
+#include <vector>
+
+#include "hri_math.h"
+#include "renderer_internal/render_context.h"
+#include "renderer_internal/buffer.h"
+
 using namespace hri;
+
+GPUMesh GPUMesh::init(RenderContext* ctx, const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices)
+{
+	assert(ctx != nullptr);
+
+	size_t indexBufferByteSize = sizeof(uint32_t) * indices.size();
+	size_t vertexBufferByteSize = sizeof(Vertex) * vertices.size();
+
+	GPUMesh mesh = GPUMesh{};
+	mesh.indexCount = indices.size();
+	mesh.vertexCount = vertices.size();
+	mesh.indexBuffer = BufferResource::init(ctx, indexBufferByteSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT, true);
+	mesh.vertexBuffer = BufferResource::init(ctx, vertexBufferByteSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, true);
+
+	mesh.indexBuffer.copyToBuffer(ctx, indices.data(), indexBufferByteSize);
+	mesh.vertexBuffer.copyToBuffer(ctx, vertices.data(), vertexBufferByteSize);
+
+	return mesh;
+}
+
+void GPUMesh::destroy(RenderContext* ctx, GPUMesh& mesh)
+{
+	BufferResource::destroy(ctx, mesh.indexBuffer);
+	BufferResource::destroy(ctx, mesh.vertexBuffer);
+
+	memset(&mesh, 0, sizeof(GPUMesh));
+}
 
 Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices)
 	:
@@ -8,4 +42,9 @@ Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<uint32_t>& ind
 	indices(indices)
 {
 	//
+}
+
+GPUMesh Mesh::createGPUMesh(RenderContext* ctx)
+{
+	return GPUMesh::init(ctx, vertices, indices);
 }
